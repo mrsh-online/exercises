f18bc6dee23d42792b12736b5a28b33b9f087fcc
shango-wk <shango.walungua@gmail.com>
2025-10-22 05:03:27 +0200

    exercices web scraping

---
+++ python/05scrapping/exo.py
@@ -0,0 +1,33 @@
+from bs4 import BeautifulSoup
+import pandas as pd
+import requests
+
+url = "https://www.bbc.com"
+response = requests.get(url)
+
+if response.status_code == 200:
+    print('Page fetch successfully !')
+else:
+    print('an error happenned')
+
+soup = BeautifulSoup(response.text, 'html.parser' )
+
+
+
+headlines = soup.find_all("a")
+
+news_data =[]
+
+
+for headline in headlines:
+    if headline.find('h2'):
+        title = headline.get_text(strip=True)
+        link = headline.get('href')
+        if  not link.startswith('http'):
+            link = f'https://bbc.com/{link}'
+        news_data.append({'title':title, 'link': link})
+
+df = pd.DataFrame(news_data)
+df.to_csv('bbc.csv', index=False)
+print('Headlines save to csv')
+
